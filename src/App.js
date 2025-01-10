4df77a15065bb8145c965d25bd8573e90a8ea824
Madhubala24 <madhu24321@gmail.com>
2025-01-10 11:10:27 +0530

    1st update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GamifiedPortfolio from './components/GamifiedPortfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <GamifiedPortfolio />
+    </div>
+  );
+}
+
+export default App;
